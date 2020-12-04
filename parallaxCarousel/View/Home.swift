4a11ca406e29568928b687a6e68fbdf02a564ab5
Katrin Mailyan <katin.mailyan31@gmail.com>
2020-12-05 00:32:43 +0300

    first commit

---
+++ parallaxCarousel/View/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  parallaxCarousel
+//
+//  Created by Катрин Маилян on 04.12.2020.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
