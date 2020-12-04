e2525effd7e549f82e720ff68dacc95140d6711f
Katrin Mailyan <katin.mailyan31@gmail.com>
2020-12-04 21:45:13 +0300

    Initial Commit

---
+++ parallaxCarousel/parallaxCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  parallaxCarouselApp.swift
+//  parallaxCarousel
+//
+//  Created by Катрин Маилян on 04.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct parallaxCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
